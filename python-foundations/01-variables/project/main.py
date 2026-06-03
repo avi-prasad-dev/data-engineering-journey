278b56187bc938a4d87aa442450d68c8ed7c1868
Avi Prasad <avi.prasad.dev@gmail.com>
2026-06-03 13:38:27 +0530

    Completed topic 01-Variables with project

---
+++ python-foundations/01-variables/project/main.py
@@ -0,0 +1,29 @@
+'''
+Mini Python Project
+Project Name
+
+Student Information Card
+Goal
+
+Store and display student details.
+Features
+- Name
+- Age
+- City
+- Email
+- Course
+
+'''
+
+student_name: str = "Ravi Kumar"
+student_age: int = 35
+student_city: str = "Dehradun"
+student_email: str = "ravikumar@gmail.com"
+student_course: str = "Masters in Computer Application"
+
+print(f"Student Name is {student_name}, age is {student_age}, lives in city {student_city}, email id is {student_email}, and course applied {student_course}")
+
+
+
+
+

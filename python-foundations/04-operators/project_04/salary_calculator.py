b23d7518e2e4e6148077efe47826037d5f30cd80
Avi Prasad <avi.prasad.dev@gmail.com>
2026-06-05 13:36:41 +0530

    Create mini project solution

---
+++ python-foundations/04-operators/project_04/salary_calculator.py
@@ -0,0 +1,8 @@
+monthly_salary: float = float(input("Enter your Monthly Salary: "))
+bonus: float = float(input("Enter bouns you get: "))
+
+total_salary: float = monthly_salary + bonus
+
+print(f"Monthly Salary: {monthly_salary}")
+print(f"Bonus: {bonus}")
+print(f"Total Salary: {total_salary}")
